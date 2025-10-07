2404e3b03f0e58b187aff9c9c3c4455ac507afd7
Vivek Kumar <vivek.kumar@retransform.com>
2025-10-07 16:27:03 +0530

    new build 007

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,11 +1,12 @@
 import CategoryPageClient from "./CategoryPageClient";
 
 interface CategoryPageProps {
-  params: { category: string };
+  params: {
+    category: string;
+  };
 }
 
 export default function CategoryPage({ params }: CategoryPageProps) {
-  const { category } = params;
-
+  const { category } = params; // Access params directly
   return <CategoryPageClient category={category} />;
 }
