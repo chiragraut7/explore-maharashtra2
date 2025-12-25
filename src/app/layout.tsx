7e63c80dc3da9f32c7e88c5b5d7a5bdf87d2e5cb
Vivek Kumar <vivek.kumar@retransform.com>
2025-12-25 14:59:11 +0530

    25122025-4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'aos/dist/aos.css'
 import 'leaflet/dist/leaflet.css'
 
