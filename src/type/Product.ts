220d4d636a795d562ab00a4d145f008d31272e17
Vivek Kumar <vivek.kumar@retransform.com>
2026-03-02 12:51:42 +0530

    02032026-3

--- src/type/Product.ts
+++ src/type/Product.ts
@@ -1,7 +1,7 @@
 export interface Product {
   name: string
   features: string[]
-  price_range: string
+  price_range: string 
   material: string
   custom_logo: boolean
   image_gallery: string[]
